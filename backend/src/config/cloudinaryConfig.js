56c075e2e9fd009f0d7ddebbdee62385f75da337
gauravPanditr <gauravpandit7856@gmail.com>
2024-09-17 14:08:53 +0530

    create login and crate employee api

--- backend/src/config/cloudinaryConfig.js
+++ backend/src/config/cloudinaryConfig.js
@@ -1,12 +1,12 @@
 
 const cloudinary = require('cloudinary').v2;
 
-//const { CLOUD_NAME,API_KEY,API_SECRET}=require('./serverConfig');
+const { CLOUD_NAME,API_KEY,API_SECRET}=require('./serverConfig');
 
 cloudinary.config({
-      cloud_name: "deeqnmgfg",
-      api_key:836845984119691,
-      api_secret:"8M3j3T7CqHUXy0SDhZh3CysjrP0"
+      cloud_name: CLOUD_NAME,
+      api_key:API_KEY,
+      api_secret:API_SECRET
 });
 module.exports=cloudinary;
 
